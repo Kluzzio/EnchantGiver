cff1c47ffdc136c8fa1639544b86aa878de2f13a
DeatHunter <desuhansa@gmail.com>
2022-01-13 02:10:12 +0100

    Minor changes

--- src/main/java/wraith/enchant_giver/Config.java
+++ src/main/java/wraith/enchant_giver/Config.java
@@ -35,7 +35,6 @@ public class Config {
         }
         file.setReadable(true);
         file.setWritable(true);
-        file.setExecutable(true);
         if (contents == null || "".equals(contents)) {
             return;
         }
