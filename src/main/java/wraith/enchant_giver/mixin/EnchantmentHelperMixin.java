149ce8019bcaac5dafd0dc9160cceb47486d10c7
Kluzzio <kluzzio.amp@gmail.com>
2022-04-02 09:52:22 -0700

    Fixed #5, Version Bump  - Added lazy steal code from Mojang getLevel method and return max.  - Is return max between normal getLevel code from EnchantmentHelper and existing 'calculation'.  - getLevel is stolen to rewrite to avoid endless recursion because it is mixed into.  - Tested if power 1 bow (given with enchant giver) is overwritten (correctly grabbed) by a power 5 book. It is.  - Tested if two soul fists (enchant giver level 1 modded enchantment on a weapon) would combine properly in an anvil. They do.  - The errors were here when I got here. Doesn't break things for some reason. Above my pay grade.

--- src/main/java/wraith/enchant_giver/mixin/EnchantmentHelperMixin.java
+++ src/main/java/wraith/enchant_giver/mixin/EnchantmentHelperMixin.java
@@ -7,7 +7,9 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -18,22 +20,51 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Mixin(EnchantmentHelper.class)
-public class EnchantmentHelperMixin {
+public abstract class EnchantmentHelperMixin {
 
     @Inject(method = "getLevel", at = @At("HEAD"), cancellable = true)
     private static void getLevel(Enchantment enchantment, ItemStack stack, CallbackInfoReturnable<Integer> cir) {
         NbtCompound nbtEnchants = stack.getSubNbt("EnchantGiver");
         Identifier enchant = Registry.ENCHANTMENT.getId(enchantment);
         if (nbtEnchants != null && enchant != null && nbtEnchants.contains(enchant.toString())) {
-            cir.setReturnValue(nbtEnchants.getInt(enchant.toString()));
+            int j = 0;
+            if (!stack.isEmpty()) {
+                Identifier identifier = EnchantmentHelper.getEnchantmentId(enchantment);
+                NbtList nbtList = stack.getEnchantments();
+
+                for (int i = 0; i < nbtList.size(); i++) {
+                    NbtCompound nbtCompound = nbtList.getCompound(i);
+                    Identifier identifier2 = EnchantmentHelper.getIdFromNbt(nbtCompound);
+                    if (identifier2 != null && identifier2.equals(identifier)) {
+                        j = EnchantmentHelper.getLevelFromNbt(nbtCompound);
+                    }
+                }
+
+            }
+            cir.setReturnValue(Math.max(j, nbtEnchants.getInt(enchant.toString())));
             return;
         }
         if (EnchantsList.itemHasEnchantment(Registry.ITEM.getId(stack.getItem()), enchant)) {
-            cir.setReturnValue(EnchantsList.getEnchantmentLevel(Registry.ITEM.getId(stack.getItem()), Registry.ENCHANTMENT.getId(enchantment)));
+            int j = 0;
+            if (!stack.isEmpty()) {
+                Identifier identifier = EnchantmentHelper.getEnchantmentId(enchantment);
+                NbtList nbtList = stack.getEnchantments();
+
+                for (int i = 0; i < nbtList.size(); i++) {
+                    NbtCompound nbtCompound = nbtList.getCompound(i);
+                    Identifier identifier2 = EnchantmentHelper.getIdFromNbt(nbtCompound);
+                    if (identifier2 != null && identifier2.equals(identifier)) {
+                        j = EnchantmentHelper.getLevelFromNbt(nbtCompound);
+                    }
+                }
+
+            }
+            cir.setReturnValue(Math.max(j,EnchantsList.getEnchantmentLevel(Registry.ITEM.getId(stack.getItem()), Registry.ENCHANTMENT.getId(enchantment))));
         }
     }
 
-    @ModifyVariable(method = "forEachEnchantment(Lnet/minecraft/enchantment/EnchantmentHelper$Consumer;Lnet/minecraft/item/ItemStack;)V", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/ItemStack;getEnchantments()Lnet/minecraft/nbt/NbtList;"))
+    @ModifyVariable(method = "forEachEnchantment(Lnet/minecraft/enchantment/EnchantmentHelper$Consumer;Lnet/minecraft/item/ItemStack;)V",
+            at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/ItemStack;getEnchantments()Lnet/minecraft/nbt/NbtList;"))
     private static NbtList forEachEnchantment(NbtList listTag, EnchantmentHelper.Consumer consumer, ItemStack stack) {
         NbtList newListTag = new NbtList();
         HashMap<String, Integer> enchantMap = new HashMap<>();
